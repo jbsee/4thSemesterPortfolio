c300dc19c18445191170d026fe77bf175dca3aa0
jbsee <jbseeberg@gmail.com>
2025-08-20 16:43:02 +0200

    blah

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "",
+    pageTitle: "Portfolio",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
