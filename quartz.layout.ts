37cd87e6267ef826185718dd00580d4a18b7282d
jbsee <jbseeberg@gmail.com>
2025-08-20 17:13:02 +0200

    fix3

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -62,7 +62,9 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "Portfolio"
+    }),
   ],
   right: [],
 }
